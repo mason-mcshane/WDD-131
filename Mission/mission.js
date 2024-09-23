1ad1d1fd7d8d16ef1223b3954b9b1de37918eb5b
mason-mcshane <mcs20002@byui.edu>
2024-09-22 22:27:02 -0600

    polished JS

--- Mission/mission.js
+++ Mission/mission.js
@@ -1,20 +1,16 @@
 const selectElement = document.getElementById('mySelect');
 
-// Function to change the theme
 function changeTheme() 
 {
-    // Get the selected option from the theme selector
     const themeSelector = document.getElementById('theme-selector');
     const selectedTheme = themeSelector.value;
 
-    // Get the body element and logo image element
     const body = document.body;
     const logo = document.getElementById('logo');
 
-    // Check the selected theme and apply changes accordingly
     if (selectedTheme === 'dark') {
         body.classList.add('dark');
-        logo.src = 'byui-logo_blue.webp';
+        logo.src = 'byui-logo_white.png';
     } else {
         body.classList.remove('dark');
         logo.src = 'byui-logo_blue.webp';
